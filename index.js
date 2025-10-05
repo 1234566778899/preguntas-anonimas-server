4b2817323ba4ef956f05352d1f326e70bb07e5eb
ordazhoyos2001@gmail.com <ordazhoyos2001@gmail.com>
2025-10-04 20:14:59 -0500

    cambios v2

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.json({
     nombre: 'Preguntas Anónimas API v3.0',
-    version: '2.0.0',
+    version: '3.0.0',
     salasActivas: salas.size,
   });
 });
